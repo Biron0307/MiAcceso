d38237b966537149ded76fbdc3be24628a1262eb
Biron0307 <bironparada8@gmail.com>
2025-08-20 19:32:43 -0600

    Add files via upload

--- login.js
+++ login.js
@@ -1,11 +1,12 @@
-document.getElementoById("LoginForm").addEventListener("Submit",function(event){event.preventDefault();
-    var email = document.getElementoById("email").value;
-    var password = document.getElementoById("password").value;
+document.getElementById("loginform").addEventListener("submit", function(event) {
+    event.preventDefault();
 
-    if (email==="eber@gmail.com" && password==="123"){
-        window.location.href="https://es.wikipedia.org/wiki/Wiki"
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    if (email === "eber@gmail.com" && password === "123") {
+        window.location.href = "https://es.wikipedia.org/wiki/Wiki";
+    } else {
+        document.getElementById("message").innerHTML = "Acceso Denegado.";
     }
-    else {
-        document.getElementoById("message").innerHTML = "Acceso Denegado.";
-    }
-    });
+});
